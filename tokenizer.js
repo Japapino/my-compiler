f6edb11d39da2fc403bf781d26eb8e4b3e0aea06
Stefan M. <stefan.murakami@gmail.com>
2025-01-07 18:28:26 -0600

    added first set of params for tokenizer

--- tokenizer.js
+++ tokenizer.js
@@ -1,10 +1,54 @@
+const LETTERS = /[a-z]/i;
+const WHITESPACE = /\s/;
+const NUMBERS = /\d/;
+
 function tokenizer(input) {
   const tokens = [];
 
   let current = 0;
-  while (curent < input.length) {
-    const char = input[current];
+  while (current < input.length) {
+    let char = input[current];
+    if (["(", ")"].includes(char)) {
+      tokens.push({ type: "paren", value: char });
+      current++;
+      continue;
+    }
+
+    if (LETTERS.test(char)) {
+      let value = "";
+
+      while (LETTERS.test(char)) {
+        value += char;
+        char = input[++current];
+      }
+
+      tokens.push({ type: "name", value: value });
+      continue;
+    }
+
+    if (WHITESPACE.test(char)) {
+      current++;
+      continue;
+    }
+
+    if (NUMBERS.test(char)) {
+      let value = "";
+
+      while (NUMBERS.test(char)) {
+        value += char;
+        char = input[++current];
+      }
+
+      tokens.push({ type: "number", value: value });
+      continue;
+    }
+
+    console.log("tokens: ", tokens);
+
+    throw new TypeError(`Unknown char: '${char}'`);
   }
 
   return tokens;
 }
+
+module.exports = tokenizer;
